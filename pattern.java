b862ce35803489cede8405e4d43b178a4725d23b
AbhinavGusain0512 <111456585+AbhinavGusain0512@users.noreply.github.com>
2022-09-30 19:44:44 +0530

    Add files via upload

--- pattern.java
+++ pattern.java
@@ -1,16 +1,21 @@
-public class Main{
-    public static void main(String[] args){
-        for(int i=0;i<3;i++){
-            for(int j=1;j<i+2;j++){
+
+
+class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 1; j < i + 2; j++) {
                 System.out.print(j);
-               
-            } System.out.println();
+
+            }
+            System.out.println();
         }
-        for(int k=3;k>=0;k--){
-            for(int l=1;l<k+2;l++){
+        for (int k = 3; k >= 0; k--) {
+            for (int l = 1; l < k + 2; l++) {
                 System.out.print(l);
-                
-            }System.out.println();
-        
+
+            }
+            System.out.println();
+
+        }
     }
-}}
+}
